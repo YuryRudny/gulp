697afaef950b57023c41e5e29ac7ddc9e711e6fe
YuryRudny <notindulget@gmail.com>
2020-12-01 11:45:05 +0600

    plane update

---
+++ src/assets/js/common.js
@@ -0,0 +1,10 @@
+import $ from 'jquery';
+import Swiper from 'swiper/bundle';
+import svg4everybody from 'svg4everybody';
+import baguetteBox from 'baguettebox.js';
+
+$(function () {
+	svg4everybody();
+
+
+});
