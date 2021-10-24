4089c45ce686a01298cb99eb66374a48265b9198
Yury Rudny <notindulget@gmail.com>
2021-10-24 10:56:23 +0600

    chore: update

--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -1,10 +1,8 @@
-import $ from 'jquery';
-import Swiper from 'swiper/bundle';
-import svg4everybody from 'svg4everybody';
-import baguetteBox from 'baguettebox.js';
+import $ from "jquery";
+import Swiper from "swiper/bundle";
+import svg4everybody from "svg4everybody";
+import baguetteBox from "baguettebox.js";
 
 $(function () {
-	svg4everybody();
-
-
+  svg4everybody();
 });
